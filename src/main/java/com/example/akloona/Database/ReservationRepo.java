440458fad5505d991da112fbb6ea0cb798d9e3e2
ebrahimAlaaeldin <ebrahim.alaa2627@yahoo.com>
2024-08-21 02:51:20 +0300

    Adding repos

---
+++ src/main/java/com/example/akloona/Database/ReservationRepo.java
@@ -0,0 +1,6 @@
+package com.example.akloona.Database;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReservationRepo extends JpaRepository<Reservation,Integer> {
+}
---
+++ src/main/java/com/example/akloona/Database/RestaurantRepo.java
@@ -0,0 +1,6 @@
+package com.example.akloona.Database;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RestaurantRepo extends JpaRepository<Restaurant,Integer> {
+}
