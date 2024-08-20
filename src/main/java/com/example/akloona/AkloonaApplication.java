a6baeae036d79db3ae19bc99177fd573420e37c9
ebrahimAlaaeldin <ebrahim.alaa2627@yahoo.com>
2024-08-20 07:07:47 +0300

    first commit

---
+++ src/main/java/com/example/akloona/AkloonaApplication.java
@@ -0,0 +1,13 @@
+package com.example.akloona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkloonaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkloonaApplication.class, args);
+    }
+
+}
