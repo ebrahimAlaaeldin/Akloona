1972ff11f377e8e270df6b7e23ee4f706f2f7b2b
ebrahimAlaaeldin <ebrahim.alaa2627@yahoo.com>
2024-08-20 07:07:47 +0300

    first commit

---
+++ src/test/java/com/example/akloona/AkloonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.akloona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkloonaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
