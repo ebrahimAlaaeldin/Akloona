f334ccaaa31229d91403e02bd625bd466cc833a4
Tareq11Mz <tareqmedhet@gmail.com>
2024-08-22 15:33:10 +0300

    regex for email - phone- password, and email notification structure, files affected -> both Resources files, user_, UserService, EmailService, Pom.xml (update project for dependencies

--- src/main/java/com/example/akloona/Database/User_.java
+++ src/main/java/com/example/akloona/Database/User_.java
@@ -1,7 +1,9 @@
 package com.example.akloona.Database;
 
-
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -20,40 +22,52 @@ public class User_ {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int ID;
+
     @Column(name = "username", nullable = false, unique = true)
+    @NotBlank(message = "Username is mandatory")
     private String username;
+
     @Column(name = "password", nullable = false)
+    @NotBlank(message = "Password is mandatory")
     private String password;
+
     @Column(name = "email", nullable = false, unique = true)
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is mandatory")
     private String email;
+
     @Column(name = "phoneNumber", nullable = false, unique = true, length = 11)
+    @Pattern(regexp = "01[0125]\\d{8}", message = "Invalid phone number format")
     private String phoneNumber;
+
     @Column(name = "address", nullable = false, columnDefinition = "TEXT")
+    @NotBlank(message = "Address is mandatory")
     private String address;
+
     @Column(name = "accountType", nullable = false)
+    @NotBlank(message = "Account type is mandatory")
     private String accountType;
 
     @Column(name = "Dob", nullable = false)
-    private  int YearOfBirth;
+    private int YearOfBirth;
+
     @Column(name = "age", nullable = false)
     private int age;
 
-
-    public User_(String username, String password, String email, String phoneNumber, String address, String acountType, int dob) {
+    public User_(String username, String password, String email, String phoneNumber, String address, String accountType, int dob) {
         this.username = username;
         this.password = password;
         this.email = email;
         this.phoneNumber = phoneNumber;
         this.address = address;
-        this.accountType = acountType;
+        this.accountType = accountType;
         this.YearOfBirth = dob;
-
+        this.calculateAge();
     }
 
-    public void calculateAge(){
+    public void calculateAge() {
         this.age = LocalDate.now().getYear() - this.YearOfBirth;
     }
-
 
     public int getId() {
         return ID;
