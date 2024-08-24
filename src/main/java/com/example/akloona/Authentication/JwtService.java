9af81ed9a53718f4993e6dfc7155d45e56d178a3
omarelansary <73857229+omarelansary@users.noreply.github.com>
2024-08-24 14:30:14 +0300

    fix: error removing added semi colon

--- src/main/java/com/example/akloona/Authentication/JwtService.java
+++ src/main/java/com/example/akloona/Authentication/JwtService.java
@@ -8,7 +8,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
-;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
